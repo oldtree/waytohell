6e5f277aee746b466f62a43a11e49e04e3d0f460
Grapes-tree <treeui.old@gmail.com>
2018-03-06 23:18:21 +0800

    war is hell

---
+++ gesture/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext contxt) {
+    return new GestureDetector(
+      onTap: (){
+        print("mysq button is clicked");
+      },
+      child: new Container(
+        height: 40.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.blue[500],
+        ),
+        child: new Center(
+          child: new Text("社会"),
+        ),
+      ),
+    )
+  }
+}
