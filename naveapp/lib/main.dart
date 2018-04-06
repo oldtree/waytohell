d4ee2d3033dad2ba50c5089ccf7be8df4a4eefeb
Grapes-tree <treeui.old@gmail.com>
2018-04-06 23:15:01 +0800

    nav route examples

--- naveapp/lib/main.dart
+++ naveapp/lib/main.dart
@@ -11,6 +11,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: new HomePage(title: '应用程序首页'),
+      routes: <String,WidgetBuilder>{
+        "/a":(BuildContext context){return new SubPage(title: "A page");},
+        "/b":(BuildContext context){return new SubPage(title: "B page");},
+        "/c":(BuildContext context){return new SubPage(title: "C page");},
+      },
     );
   }
 }
@@ -54,13 +59,53 @@ class DefaultHomePage extends State<HomePage>{
     return  new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
+        
       ),
       body: new Center(
-        child: new Text("click to start"),
+        child:new Row(
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text("A page"),
+              onPressed: (){
+                Navigator.of(context).pushNamed("/a");
+              },
+            ),
+            new RaisedButton(
+              child: new Text("B page"),
+              onPressed: (){
+                Navigator.of(context).pushNamed("/b");
+              },
+            ),
+            new RaisedButton(
+              child: new Text("C page"),
+              onPressed: (){
+                Navigator.of(context).pushNamed("/c");
+              },
+            )
+          ],
+        ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: OpenNewPage,
-        child: new Icon(Icons.open_in_new),
+    );
+  }
+}
+
+class SubPage extends StatelessWidget{
+  SubPage({Key key,this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Text(
+          title,
+          style: new TextStyle(
+            fontSize: 20.0,
+            decorationColor: Colors.red[100],
+          ),
+        ),
       ),
     );
   }
